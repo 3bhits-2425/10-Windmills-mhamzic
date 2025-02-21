0d22db1106f5a053ff2391afcf5d4c666c6f0e94
hamzic08 <muhammed.hamzic08@htl-salzburg.ac.at>
2025-02-22 00:30:19 +0100

    windmills

--- Assets/MyGame/Scripts/WindmillDynamicSpeed.cs
+++ Assets/MyGame/Scripts/WindmillDynamicSpeed.cs
@@ -1,29 +1,42 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class WindmillGameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] windmills; 
-    [SerializeField] private Slider[] windmillSliders; 
-    [SerializeField] private Button lockButton; 
-    [SerializeField] private Button applyColorButton; 
-    [SerializeField] private GameObject colorTarget; 
+    [SerializeField] private GameObject[] windmills;
+    [SerializeField] private Slider[] windmillSliders;
+    [SerializeField] private Button lockButton;
+    [SerializeField] private Button applyColorButton;
+    [SerializeField] private GameObject colorTarget;
     private int currentWindmillIndex = 0;
-    private float[] windmillSpeeds = new float[3]; 
+    private float[] windmillSpeeds = new float[3];
     private bool[] isLocked = new bool[3];
     private bool allLocked = false;
     private float maxRotationSpeed = 255f;
-    private float decreaseRate = 100f; 
+    private float decreaseRate = 100f;
+
+    [SerializeField] private Light lamp;
+    [SerializeField] private Slider lampSlider;
+    [SerializeField] private TextMeshPro rgb;
+    [SerializeField] private Button[] buttons;
 
     private void Start()
     {
         lockButton.onClick.AddListener(LockCurrentWindmill);
         applyColorButton.onClick.AddListener(ApplyColor);
+
+        lamp.intensity = 0;
+        buttons[1].interactable = false;
+        buttons[2].interactable = false;
+
+        buttons[0].onClick.AddListener(() => buttons[1].interactable = true);
+        buttons[1].onClick.AddListener(() => buttons[2].interactable = true);
     }
 
     private void Update()
     {
-        if (!allLocked && currentWindmillIndex < windmills.Length)
+        if (currentWindmillIndex < windmills.Length)
         {
             if (!isLocked[currentWindmillIndex])
             {
@@ -38,18 +51,24 @@ public class WindmillGameManager : MonoBehaviour
             }
         }
         RotateWindmills();
+
+        lamp.intensity = lampSlider.value;
+        rgb.text = lamp.intensity.ToString("F2");
     }
 
     private void IncreaseWindmillValue(float deltaTime)
     {
-        float newValue = Mathf.Clamp(windmillSpeeds[currentWindmillIndex] + (deltaTime * 100f), 0, maxRotationSpeed);
-        windmillSpeeds[currentWindmillIndex] = newValue;
-        windmillSliders[currentWindmillIndex].value = newValue;
+        if (currentWindmillIndex < windmills.Length && !isLocked[currentWindmillIndex])
+        {
+            float newValue = Mathf.Clamp(windmillSpeeds[currentWindmillIndex] + (deltaTime * 100f), 0, maxRotationSpeed);
+            windmillSpeeds[currentWindmillIndex] = newValue;
+            windmillSliders[currentWindmillIndex].value = newValue;
+        }
     }
 
     private void DecreaseWindmillValue(float deltaTime)
     {
-        if (!isLocked[currentWindmillIndex])
+        if (currentWindmillIndex < windmills.Length && !isLocked[currentWindmillIndex])
         {
             float newValue = Mathf.Clamp(windmillSpeeds[currentWindmillIndex] - (deltaTime * decreaseRate), 0, maxRotationSpeed);
             windmillSpeeds[currentWindmillIndex] = newValue;
@@ -61,8 +80,11 @@ public class WindmillGameManager : MonoBehaviour
     {
         for (int i = 0; i < windmills.Length; i++)
         {
-            float speed = windmillSpeeds[i];
-            windmills[i].transform.Rotate(0, 0, speed * Time.deltaTime);
+            if (i == currentWindmillIndex && isLocked[i])
+            {
+                float speed = windmillSpeeds[i];
+                windmills[i].transform.Rotate(0, 0, speed * Time.deltaTime);
+            }
         }
     }
 
@@ -71,11 +93,10 @@ public class WindmillGameManager : MonoBehaviour
         if (currentWindmillIndex < windmills.Length && !isLocked[currentWindmillIndex])
         {
             isLocked[currentWindmillIndex] = true;
-            
-            
             if (currentWindmillIndex < windmills.Length - 1)
             {
                 currentWindmillIndex++;
+                windmillSliders[currentWindmillIndex].interactable = true;
             }
             else
             {
--- Assets/MyGame/Scripts/windmills.cs
+++ Assets/MyGame/Scripts/windmills.cs
@@ -1,25 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class Windmills : MonoBehaviour
 {
-
     [SerializeField] private Light lamp;
     [SerializeField] private Slider slider;
     [SerializeField] private TextMeshPro rgb;
- 
+    [SerializeField] private Button[] buttons;
 
     void Start()
     {
         lamp.intensity = 0;
+        buttons[1].interactable = false;
+        buttons[2].interactable = false;
+
+        buttons[0].onClick.AddListener(() => buttons[1].interactable = true);
+        buttons[1].onClick.AddListener(() => buttons[2].interactable = true);
     }
+
     void Update()
     {
         lamp.intensity = slider.value;
-        rgb.text = lamp.intensity.ToString();
+        rgb.text = lamp.intensity.ToString("F2");
     }
 }
