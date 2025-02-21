c7c84d9b5c40d153fa37e876dd2504350907ea1e
hamzic08 <muhammed.hamzic08@htl-salzburg.ac.at>
2025-02-21 10:44:38 +0100

    windmills

--- Assets/MyGame/Scripts/windmills.cs
+++ Assets/MyGame/Scripts/windmills.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Windmills : MonoBehaviour
 {
 
     [SerializeField] private Light lamp;
     [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshPro rgb;
+ 
 
     void Start()
     {
-        lamp.intensity = 0; 
-        
+        lamp.intensity = 0;
     }
     void Update()
     {
-       lamp.intensity = slider.value;
+        lamp.intensity = slider.value;
+        rgb.text = lamp.intensity.ToString();
     }
 }
